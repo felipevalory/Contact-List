0181f180aec00525bf6c7576067fd53737e2d398
Felipe <felipevalory@gmail.com>
2023-12-21 12:02:43 -0300

    Inserindo view de contato unico

--- contact/admin.py
+++ contact/admin.py
@@ -4,14 +4,14 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone',
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
     # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 100
-    # list_editable = 'first_name',
-    list_display_links = 'id', 'first_name',
+    list_editable = 'first_name', 'last_name', 'show',
+    list_display_links = 'id',
 
 
 @admin.register(models.Category)
--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from contact import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -7,6 +7,7 @@ from django.conf import settings
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', views.contact, name='contact'),
     path('', views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
