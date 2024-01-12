eb4967d1166d05febdffe79250ca55662bfac1a2
Felipe <felipevalory@gmail.com>
2024-01-11 21:37:44 -0300

    ImageField no forms e template

--- contact/forms.py
+++ contact/forms.py
@@ -4,11 +4,20 @@ from django.core.exceptions import ValidationError
 
 
 class ContactForm(forms.ModelForm):
+    picture = forms.ImageField(
+       widget=forms.FileInput(
+          attrs={
+             'accept': 'image/*',
+          }
+       )
+    )
+
     class Meta:
         model = models.Contact
         fields = (
            'first_name', 'last_name', 'phone',
            'email', 'description', 'category',
+           'picture',
         )
 
 
