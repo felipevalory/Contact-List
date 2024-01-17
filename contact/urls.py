33613019e108c096980b94f007e6c832865a77ba
Felipe <felipevalory@gmail.com>
2024-01-17 15:53:21 -0300

    Criando sistema de login e logout

--- contact/urls.py
+++ contact/urls.py
@@ -17,8 +17,10 @@ urlpatterns = [
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 
-    # Contact (CRUD)
+    # User
     path('user/create/', views.register, name='register'),
+    path('user/login/', views.login_view, name='login'),
+    path('user/logout/', views.logout_view, name='logout'),
 
 ]
 
