ca65a85e33fc126915520672eb28bc46164547fa
Felipe <felipevalory@gmail.com>
2024-01-17 16:54:28 -0300

    Diferenciando usuario logado e não logado

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from contact.forms import ContactForm
 from contact.models import Contact
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
 
@@ -37,6 +40,7 @@ def create(request):
     )
 
 
+@login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
     form_action = reverse('contact:update', args=(contact_id,))
@@ -72,6 +76,7 @@ def update(request, contact_id):
     )
 
 
+@login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(Contact, pk=contact_id, show=True)
     
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -3,6 +3,7 @@ from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     form = RegisterForm()
@@ -47,6 +48,7 @@ def login_view(request):
     )
 
 
+@login_required(login_url='contact:login')
 def user_update(request):
 
     form = RegisterUpdateForm(instance=request.user)
@@ -76,7 +78,7 @@ def user_update(request):
     return redirect('contact:user_update')
          
 
-
+@login_required(login_url='contact:login')
 def logout_view(request):
      auth.logout(request)
      return redirect('contact:login')
