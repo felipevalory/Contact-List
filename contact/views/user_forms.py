3d6ba5f8a00e6c31c1da1828730bf05d5f835ee9
Felipe <felipevalory@gmail.com>
2024-01-17 16:27:36 -0300

    Criando formulário para atualizar usuário

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
@@ -45,6 +45,36 @@ def login_view(request):
             'form': form
         }
     )
+
+
+def user_update(request):
+
+    form = RegisterUpdateForm(instance=request.user)
+
+    if request.method != 'POST':
+         
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if form.is_valid():
+          return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form
+            }
+        )
+    
+    form.save()
+    return redirect('contact:user_update')
+         
 
 
 def logout_view(request):
